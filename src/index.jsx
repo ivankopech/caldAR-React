087f0efa480572987ce203b818b124239f1219a0
ivan kopech <ivankopech@gmail.com>
2021-11-12 08:29:51 -0300

    agrego configuracion de thunk

--- src/index.jsx
+++ src/index.jsx
@@ -3,10 +3,24 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { composeWithDevTools } from 'redux-devtools-extension';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import thunk from 'redux-thunk';
+import rootReducer from './redux/reducers/rootReducer';
+
+const configureStore = () => {
+  const enhancer = composeWithDevTools(applyMiddleware(thunk));
+  return createStore(rootReducer, enhancer);
+};
+
+const store = configureStore();
 
 ReactDOM.render(
   <React.StrictMode>
+    <Provider store = {store}>
     <App />
+    </Provider>
   </React.StrictMode>,
   document.getElementById('root')
 );
